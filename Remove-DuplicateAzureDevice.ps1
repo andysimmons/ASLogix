fcccd89faa125900ff07f8ef83c23ffd38931363
Andrew Simmons <simmonsa@slhs.org>
2018-05-07 16:41:56 -0600

    added stale AAD device cleanup script

--- Remove-DuplicateAzureDevice.ps1
+++ Remove-DuplicateAzureDevice.ps1
@@ -1,96 +1,96 @@
 #requires -Version 4
 #requires -Module AzureADPreview
-[CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+[CmdletBinding(SupportsShouldProcess)]
 param (
-	[ValidateScript({$_ -ge 0})]
-	[int]
-	$DeviceLimit = 0,
+    [ValidateScript({$_ -ge 0})]
+    [int]
+    $DeviceLimit = 0,
     
-	[ValidateScript({$_ -ge 1})]
-	[int]
-	$MaxAgeInDays = 14
+    [ValidateScript({$_ -ge 1})]
+    [int]
+    $MaxAgeInDays = 14
 )
 
 #region functions
 function Get-NonPersistentAzureADDevice
 {
-	[CmdletBinding()]
-	param (
-		[string]
-		$Pattern = 'XD[BT][PNDT]\d{2}(?!PERS).*',
+    [CmdletBinding()]
+    param (
+        [string]
+        $Pattern = 'XD[BT][PNDT]\d{2}(?!PERS).*',
 
-		[string]
-		$ODataFilter = "startswith(displayName,'xd')"
-	)
+        [string]
+        $ODataFilter = "startswith(displayName,'xd')"
+    )
 
-	Write-Verbose "Pulling list of Azure AD devices. This may take a few minutes."
-	$aadDevices = Get-AzureADDevice -Filter $ODataFilter -All:$true
+    
+    Write-Verbose "[$(Get-Date -format G)] Pulling list of Azure AD devices. This may take a few minutes."
+    $aadDevices = Get-AzureADDevice -Filter $ODataFilter -All:$true
 
-	# return the interesting devices 
-	$aadDevices.Where({$_.DisplayName -match $Pattern})
+    # return the interesting devices 
+    $aadDevices.Where({$_.DisplayName -match $Pattern})
 }
 
 function Select-StaleAzureADDevice
 {
-	[CmdletBinding()]
-	param (
-		[Parameter(Mandatory)]
-		[int]
-		$MaxAgeInDays,
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [int]
+        $MaxAgeInDays,
 
-		[Parameter(Mandatory)]
-		[Microsoft.Open.AzureAD.Model.Device[]] 
-		$Device
-	)
-	$now = Get-Date
+        [Parameter(Mandatory)]
+        [Microsoft.Open.AzureAD.Model.Device[]] 
+        $Device
+    )
+    $now = Get-Date
 
-	Write-Verbose "Grouping duplicate device objects. This may also take a few minutes."
-	$groups = $Device | Group-Object -Property 'DisplayName'
-	$i = 0
+    Write-Verbose "[$(Get-Date -format G)] Grouping duplicate device objects. This may also take a few minutes."
+    $groups = $Device | Group-Object -Property 'DisplayName'
+    $i = 0
 
-	foreach ($g in $groups)
-	{
-		$i++
-		$wpParams = @{
-			Activity        = "Checking AAD device freshness"
-			Status          = ("Group {0}/{1} ({2})" -f $i, $groups.Count, $g.Group[0].DisplayName)
-			PercentComplete = ($i / $groups.Count) * 100
-		}
-		Write-Progress @wpParams
+    foreach ($g in $groups)
+    {
+        $i++
+        $wpParams = @{
+            Activity        = "Checking AAD device freshness"
+            Status          = ("Group {0}/{1} ({2})" -f $i, $groups.Count, $g.Group[0].DisplayName)
+            PercentComplete = ($i / $groups.Count) * 100
+        }
+        Write-Progress @wpParams
 
-		$devices = @($g.Group | Sort-Object -Property 'ApproximateLastLogonTimestamp' -Descending)
+        $devices = @($g.Group | Sort-Object -Property 'ApproximateLastLogonTimestamp' -Descending)
 
-		if (($now - $devices[0].ApproximateLastLogonTimestamp).TotalDays -le $MaxAgeInDays)
-		{
-			# if the most recent record isn't stale, skip it
-			$devices | Select-Object -Skip 1
-		}
-		else { $devices }
-	}
+        if (($now - $devices[0].ApproximateLastLogonTimestamp).TotalDays -le $MaxAgeInDays)
+        {
+            # if the most recent record isn't stale, skip it
+            $devices | Select-Object -Skip 1
+        }
+        else { $devices }
+    }
 }
 
-# Wrap Remove-AzureADDevice to implement ShouldProcess
+# Wrap Remove-AzureADDevice to implement ShouldProcess (-WhatIf/-Confirm)
 function Remove-AzureADDeviceSP
 {
-	[CmdletBinding(SupportsShouldProcess)]
-	param (
-		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-		[string[]]
-		$ObjectId
-	)
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [guid[]]
+        $ObjectId
+    )
     
-	process
-	{
-		foreach ($id in $ObjectId)
-		{
-			if ($PSCmdlet.ShouldProcess($id, 'Remove')) 
-			{
-				# Remove-AzureADDevice -ObjectId $id
+    process
+    {
+        foreach ($id in $ObjectId)
+        {
+            if ($PSCmdlet.ShouldProcess($id, 'Remove')) 
+            {
+                # Remove-AzureADDevice -ObjectId $id
                 Write-Warning "Placeholder: Remove-AzureADDevice -ObjectId $id"
-			}
-		}
-	}
-    
+            }
+        }
+    }
 }
 #endregion functions
 
@@ -100,18 +100,20 @@ $staleNonPersistents = @(Select-StaleAzureADDevice -Device $npDevices -MaxAgeInD
 
 if (!$staleNonPersistents)
 {
-	'Nothing to do.'
-	exit
+    'Nothing to do.'
+    exit
 }
 
-# enforce device limit
-if ($DeviceLimit -and ($DeviceLimit -gt $staleNonPersistents.Count)) 
+# enforce device limit if specified
+if ($DeviceLimit -and ($DeviceLimit -lt $staleNonPersistents.Count)) 
 {
-	$remainder = $staleNonPersistents.Count - $DeviceLimit
-	Write-Warning "Ignorning $remainder stale AAD devices! Cleanup limited to the first $DeviceLimit."
-	$staleNonPersistents = @($staleNonPersistents | Select-Object -First $DeviceLimit)
+    $remainder = $staleNonPersistents.Count - $DeviceLimit
+    Write-Warning "Ignorning $remainder stale AAD devices! Cleanup limited to the first $DeviceLimit."
+    $staleNonPersistents = @($staleNonPersistents | Select-Object -First $DeviceLimit)
 }
 
-# call Remove-AzureADDevice wrapper function (implements -WhatIf/-Confirm)
+Write-Verbose "[$(Get-Date -format G)] Found $($staleNonPersistents.Count) device(s) eligible for removal."
+
+# remove devices (via wrapper function)
 $staleNonPersistents | Remove-AzureADDeviceSP
 #endregion main
